25b2a9e87a3d0613721a9810b1dde002c252d0f0
ItayBe <itaybe777@gmail.com>
2024-05-20 19:22:07 +0300

    Alter name of `next` to `next_node`

--- src/adt/linked_list/linked_list.py
+++ src/adt/linked_list/linked_list.py
@@ -1,14 +1,17 @@
+from typing import Any, Union
+
+
 class LinkedListNode:
-    def __init__(self, value = None, next = None):
+    def __init__(self, value: Any = None, next_node: Union['LinkedListNode', None] = None):
         self.value = value
-        self.next = next
+        self.next_node = next_node
 
     def __str__(self):
         formatted_list = ''
         temp_list = self
         while temp_list is not None:
             formatted_list += f'[{temp_list.value.__repr__()}]'
-            temp_list = temp_list.next
+            temp_list = temp_list.next_node
             if temp_list is None:
                 break
             formatted_list += ' -> '

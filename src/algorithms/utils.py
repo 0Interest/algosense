a0e4c4af0be8561b84c75d9d26c76e645fc53dc5
ItayBe <itaybe777@gmail.com>
2024-05-20 23:48:00 +0300

    Change type hinting from `list[int, ...]` -> `list[int]`

--- src/algorithms/utils.py
+++ src/algorithms/utils.py
@@ -1,6 +1,6 @@
 from typing import Any
 
 
-def swap(array: list[Any, ...], i: int, j: int) -> list[Any, ...]:
+def swap(array: list[Any], i: int, j: int) -> list[Any]:
     array[i], array[j] = array[j], array[i]
     return array

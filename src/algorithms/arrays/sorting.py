b193a8d0ae76f0af4e5a8161030b5ba7698a39cb
ItayBe <itaybe777@gmail.com>
2024-05-21 22:03:19 +0300

    Implement `merge_sort` in `sorting.py` and add `merge` utility in `utils.py`

--- src/algorithms/arrays/sorting.py
+++ src/algorithms/arrays/sorting.py
@@ -1,9 +1,18 @@
-from algorithms.utils import swap
+from algorithms.utils import swap, merge
 
 
 def bubble_sort(array: list[int]) -> list[int]:
     for i in range(len(array) - 1):
-        for j in range(len(array) - 1, i,  -1):
+        for j in range(len(array) - 1, i, -1):
             if array[i] > array[j]:
                 swap(array, i, j)
     return array
+
+
+def merge_sort(array: list[int]) -> list[int]:
+    if len(array) <= 1:
+        return array
+    middle = len(array) // 2
+    left_array = merge_sort(array[: middle])
+    right_array = merge_sort(array[middle:])
+    return merge(left_array, right_array)
--- src/algorithms/utils.py
+++ src/algorithms/utils.py
@@ -4,3 +4,24 @@ from typing import Any
 def swap(array: list[Any], i: int, j: int) -> list[Any]:
     array[i], array[j] = array[j], array[i]
     return array
+
+
+def merge(array1: list[int], array2: list[int]) -> list[int]:
+    merged_array = []
+    i, j = 0, 0
+
+    while i != len(array1) or j != len(array2):
+
+        if i == len(array1) or j == len(array2):
+            break
+
+        if array1[i] <= array2[j]:
+            merged_array.append(array1[i])
+            i += 1
+        elif array1[i] > array2[j]:
+            merged_array.append(array2[j])
+            j += 1
+
+    merged_array.extend(array1[i:])
+    merged_array.extend(array2[j:])
+    return merged_array
